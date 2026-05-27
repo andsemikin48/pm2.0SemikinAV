c05705b7ea1edf7fddb36020c92f1e88b4e7ccbb
Andre <andsemikin@yandex.ru>
2026-05-27 19:15:52 +0000

    Add DB

---
+++ animal_db_dump.sql
@@ -0,0 +1,27 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict cJ9bgMq2hNe2kGBugo7KQYd7eTuxtMEu5c3HuJEQcs5qMEUsgMuTcAaD1hq3ifq
+
+-- Dumped from database version 18.4 (Ubuntu 18.4-0ubuntu0.26.04.1)
+-- Dumped by pg_dump version 18.4 (Ubuntu 18.4-0ubuntu0.26.04.1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict cJ9bgMq2hNe2kGBugo7KQYd7eTuxtMEu5c3HuJEQcs5qMEUsgMuTcAaD1hq3ifq
+
